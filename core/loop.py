077f209c952eba4ecdd46d73f3c55cb5a6bd0767
jaishankar-2003 <jaishankar98421@gmail.com>
2026-02-21 10:57:12 +0530

    test

---
+++ core/loop.py
@@ -0,0 +1,61 @@
+n =  4
+
+for i in range (0,n):
+    print(i)
+
+li = ["geeks", "for", "geeks"]
+for x in li:
+    print(x)
+
+tup = ("geeks", "for", "geeks")
+for x in tup:
+    print(x)
+
+s = "abc"
+for x in s:
+    print(x)
+
+d = dict({'x': 123, 'y': 354})
+for x in d:
+    print("%s  %d" % (x, d[x]))
+
+set1 = {10, 30, 20}
+for x in set1:
+    print(x),
+
+
+li = ["geeks", "for", "geeks"]
+for index in range(len(li)):
+    print(li[index])
+
+la = ["geeks", "for", "geeks"]
+for index in range(len(la)):
+    print(la[index])
+
+tup = ("geeks", "for", "geeks")
+for x in tup:
+    print(x)
+
+
+for i in range (4):
+    for j in range(4):
+        print("*", end=" ")
+    print()
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+
+for i in range (1,5):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+# *
+# * *
+# * * *
+# * * * *
+
+for i in range (5,1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
